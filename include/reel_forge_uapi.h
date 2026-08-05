87c0da1637fcc3a258c31678b255983ed49d8340
Sandesh Ghimire <sandesh@soccentric.com>
2026-08-05 16:06:05 -0700

    feat: implement embedded Linux requirements - GPU accel, profiling, V4L2, FFmpeg, RTSP, WebRTC, UVC, Yocto, bootloader, watchdog, OTA, MCU, DDR tuning

--- include/reel_forge_uapi.h
+++ include/reel_forge_uapi.h
@@ -18,3 +18,21 @@ struct reel_forge_decode_config {
 #define REEL_FORGE_IOCTL_STOP_DECODE   _IO(REEL_FORGE_MAGIC, 4)
 #define REEL_FORGE_IOCTL_GET_STATUS    _IOR(REEL_FORGE_MAGIC, 5, __u32)
 #endif
+
+/* Additional codec and transport support */
+#define REEL_FORGE_CODEC_MJPEG 3
+#define REEL_FORGE_TRANSPORT_RTP    0
+#define REEL_FORGE_TRANSPORT_RTSP   1
+#define REEL_FORGE_TRANSPORT_WEBRTC 2
+#define REEL_FORGE_SOURCE_CSI  0
+#define REEL_FORGE_SOURCE_UVC  1
+
+struct reel_forge_transport_config {
+    __u32 transport;
+    __u32 port;
+    __u32 ttl;
+    __u32 reserved[4];
+};
+
+#define REEL_FORGE_IOCTL_SET_TRANSPORT _IOW(REEL_FORGE_MAGIC, 5, struct reel_forge_transport_config)
+#define REEL_FORGE_IOCTL_SET_SOURCE    _IOW(REEL_FORGE_MAGIC, 6, __u32)
